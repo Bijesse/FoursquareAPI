f06af59e76ebd61cf966214878f1552fed25c810
Tom Bijesse <bijesse.thomas@gmail.com>
2015-12-09 22:52:22 +0000

    added sams readme

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $( document ).ready(function() {
     
+    var client_id="ZQS3S2ZWFOC4ZW1HQT41U2RXKRHEMEXOIPW4OZMIYFMLITZP"
+    var client_secret="IQJ30SJKZL12BHBRYI0IHQU5FAPX4BEGZHRGH40VMLFHX4GL"
+    
     $( "#submit" ).click(function() {
         alert( "you clicked the button!" );
     });
