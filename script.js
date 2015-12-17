feb140fc7ed2ff3560e1b1cdec20a1b086ead995
Tom Bijesse <bijesse.thomas@gmail.com>
2015-12-17 16:59:58 +0000

    functional with 1 restaurant

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ $( document ).ready(function() {
     var client_id="ZQS3S2ZWFOC4ZW1HQT41U2RXKRHEMEXOIPW4OZMIYFMLITZP";
     var client_secret="IQJ30SJKZL12BHBRYI0IHQU5FAPX4BEGZHRGH40VMLFHX4GL";
     
+   
+    
     $( "#submit" ).click(function() {
             $.get("https://api.foursquare.com/v2/venues/search" +
         "?client_id=" + client_id +
         "&client_secret=" + client_secret +
         "&v=20151209" +
-        "&near=nyc" +
+        "&near=" + $("#address").val() +
         "&query=pizza",
         function(data) {
             //console.log(data);
@@ -16,7 +18,6 @@ $( document ).ready(function() {
             $('#results').append(answer);
         });
     });
-
 
 
 });
